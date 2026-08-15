c28b0997a083c6fa0d32708fbb8d9e10e2121764
Xiao <Xzj0202@outlook.com>
2026-08-15 17:59:53 +0800

    第一课完成：javac -d 与java -cp全部成功

---
+++ src/com/gdufe/lesson1/Calculator.java
@@ -0,0 +1,7 @@
+package com.gdufe.lesson1;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/com/gdufe/lesson1/Main.java
@@ -0,0 +1,7 @@
+package com.gdufe.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(3, 5));
+    }
+}
