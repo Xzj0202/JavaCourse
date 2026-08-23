5970e8f2873eecfefce70569a9e2757331325600
Xiao <Xzj0202@outlook.com>
2026-08-23 19:46:20 +0800

    8.23

---
+++ src/com/gdufe/lesson10/LeetCode_283.java
@@ -0,0 +1,45 @@
+package com.gdufe.lesson10;
+
+public class LeetCode_283 {
+    // LeetCode_283 移动零
+    public void moveZeroes(int[] nums) {
+        int p = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != 0) {
+                swap(nums, p++, i);
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        nums[i] ^= nums[j];
+        nums[j] ^= nums[i];
+        nums[i] ^= nums[j];
+    }
+
+    public static void main(String[] args) {
+        LeetCode_283 leetCode_283 = new LeetCode_283();
+        int[] nums1 = { 0, 1, 0, 3, 12 };
+        leetCode_283.moveZeroes(nums1);
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        int[] nums2 = { 0 };
+        leetCode_283.moveZeroes(nums2);
+        for (int num : nums2) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        int[] nums3 = { 1, 0, 2 };
+        leetCode_283.moveZeroes(nums3);
+        for (int num : nums3) {
+            System.out.print(num + " ");
+        }
+    }
+}
